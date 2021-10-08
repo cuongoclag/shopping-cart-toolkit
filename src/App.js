a07320c6742692689352d3c73ec3cba26cc6d7e9
chuanhoang11 <59105450+chuanhoang11@users.noreply.github.com>
2021-10-08 12:05:58 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import 'boxicons'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NavBar from "./components/Navbar"
 function App() {
   return (
     <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <h1>HELLO</h1>
+        </Switch>
+      </Router>
     </div>
   );
 }
