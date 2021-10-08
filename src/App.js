cfe935eb0f5a078dd52d6c73a6838ed861faae74
Cường Phan <cuongoclag@gmail.com>
2021-10-08 16:15:29 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import {
   Link
 } from "react-router-dom";
 import NavBar from "./components/Navbar"
+import Footer from "./components/Footer"
+import ProductList from "./components/ProductList"
+import ProductDetail from "./components/ProductDetail"
+import Cart from "./components/Cart"
+import Login from "./components/Login"
+
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Switch>
-          <h1>HELLO CHuan Hoang</h1>
+          <Route path="/" exact component={ProductList} />
+          <Route path="/product/:id" exact component={ProductDetail} />
+          <Route path="/cart" exact component={Cart} />
+          <Route path="/login" exact component={Login} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
